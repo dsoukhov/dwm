b0763ce67e533e9f193cdf915a48f12b184f9eb1
Daniel Soukhov <dsoukhov@gmail.com>
2021-05-13 06:38:42 -0400

    updated stacker config

--- config.def.h
+++ config.def.h
@@ -61,8 +61,8 @@ static const char *stack_symbols[] = { "∨", "∧"};
 
 #define STACKKEYS(MOD,ACTION) \
 	{ MOD, XK_j,     ACTION##stack, {.i = INC(+1) } }, \
-	{ MOD, XK_k,     ACTION##stack, {.i = INC(-1) } }, \
-	{ MOD, XK_grave, ACTION##stack, {.i = PREVSEL } }, \
+	{ MOD, XK_k,    ACTION##stack, {.i = INC(-1) } }, \
+	{ MOD, XK_w,     ACTION##stack, {.i = PREVSEL } }, \
 	{ MOD, XK_q,     ACTION##stack, {.i = 0 } }, \
 	{ MOD, XK_a,     ACTION##stack, {.i = 1 } }, \
 	{ MOD, XK_z,     ACTION##stack, {.i = 2 } }, \
