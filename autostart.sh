c09d5f824ae78a0f2c9db5629aba4a479da4f894
Daniel Soukhov <dsoukhov@gmail.com>
2021-06-19 06:50:56 -0400

    updated autostart

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+#feh --bg-scale ~/Pictures/wallpaper/wp1.png &
+compton --vsync opengl-swc --backend glx -e 1.0 -i 1.0 -o 1.0 -b
+xsettingsd -c ~/.xsettingsd &
 dunst &
 udiskie &
 dwmblocks &
