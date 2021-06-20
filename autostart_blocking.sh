87901fe5e2468c817861093c50e9859e8bfb5425
Daniel Soukhov <dsoukhov@gmail.com>
2021-06-19 23:35:38 -0400

    made autostart nonblocking

--- autostart_blocking.sh
+++ autostart_blocking.sh
@@ -1,5 +1,1 @@
 #!/bin/sh
-pamixer --set-volume 40
-pamixer --source "alsa_input.pci-0000_00_1f.3.analog-stereo" --set-volume 20
-xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
-hsetroot -solid '#000000'
