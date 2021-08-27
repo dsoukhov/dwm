2c02312ddc755056b2df3f55bb825811fe8f3a1d
Daniel Soukhov <dsoukhov@gmail.com>
2021-08-27 13:27:42 -0400

    applied xresources patch

--- config.def.h
+++ config.def.h
@@ -1,30 +1,33 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
-static const unsigned int gappx     = 3;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack Nerd Font Mono:size=9", "Noto Color Emoji:style=Regular:pixelsize=12:antialias=true:autohint=true" };
-static const char dmenufont[]       = "Hack Nerd Font Mono:size=9";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char col_urgborder[]   = "#ff0000";
-static const char *colors[][3]      = {
-  /*               fg         bg         border   */
-  [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-  [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-  [SchemeUrg]  = { col_gray4, col_cyan,  col_urgborder},
-};
+static unsigned int borderpx        = 4;   /* border pixel of windows */
+static unsigned int gappx           = 3;   /* gaps between windows */
+static unsigned int snap            = 32;  /* snap pixel */
+static unsigned int systraypinning  = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static unsigned int systrayonleft   = 0;   /* 0: systray in the right corner, >0: systray on left of status text */
+static unsigned int systrayspacing  = 2;   /* systray spacing */
+static int systraypinningfailfirst  = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;   /* 0 means no systray */
+static const int showbar            = 1;   /* 0 means no bar */
+static int topbar                   = 1;   /* 0 means bottom bar */
+static char font[]                  = "Hack Nerd Font Mono:size=9";
+static char font2[]                 = "Noto Color Emoji:style=Regular:pixelsize=12:antialias=true:autohint=true";
+static const char *fonts[]          = { font, font2 };
+static char dmenufont[]             = "Hack Nerd Font Mono:size=9";
+static char normbgcolor[]           = "#222222";
+static char normbordercolor[]       = "#444444";
+static char normfgcolor[]           = "#bbbbbb";
+static char selfgcolor[]            = "#eeeeee";
+static char selbordercolor[]        = "#005577";
+static char selbgcolor[]            = "#005577";
+static char urgborder[]             = "#ff0000";
+static char *colors[][3]            = {
+                                        /*               fg           bg           border   */
+                                        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+                                        [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+                                        [SchemeUrg]  = { selfgcolor,  selbgcolor,  urgborder },
+                                      };
 
 /* sticky symbol */
 static const XPoint stickyicon[]    = { {0,0}, {4,0}, {4,8}, {2,6}, {0,8}, {0,0} }; /* represents the icon as an array of vertices */
@@ -37,7 +40,6 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const int swaldecay = 3;
 static const int swalretroactive = 1;
 static const XPoint swaliconbb = {2,16};	/* defines the bottom right corner of the polygon's bounding box (speeds up scaling) */
-//static const char swalsymbol[] = ">O";
 
 static const Rule rules[] = {
   /* xprop(1):
@@ -51,16 +53,17 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static int nmaster     = 1;    /* number of clients in master area */
+static int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
-static const int defaultatchdir = 0;  /*    set the default attach dir
+static const int defaultatchdir = 0;
+                            /*    set the default attach dir
                                   0, new goes below focused
                                   1, new goes bottommost
                                   2, new goes above focused
                                   3, new goes above master
-                             */
+                            */
 static const char *stack_symbols[] = { "*∨", "∨", "*∧", "∧" };
 
 static const Layout layouts[] = {
@@ -70,7 +73,6 @@ static const Layout layouts[] = {
   { "=[]",      lefttile },
   { "[D]",      deck },
   { "><>",      NULL },    /* no layout function means floating behavior */
-  //{ "[M]",      monocle },
 };
 
 /* key definitions */
@@ -78,12 +80,11 @@ static const Layout layouts[] = {
 #define TAGKEYS(KEY,TAG) \
   { MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
   { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-  { MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-  //{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+  { MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} },
 
 #define STACKKEYS(MOD,ACTION) \
   { MOD, XK_j,     ACTION##stack, {.i = INC(+1) } }, \
-  { MOD, XK_k,    ACTION##stack, {.i = INC(-1) } }, \
+  { MOD, XK_k,     ACTION##stack, {.i = INC(-1) } }, \
   { MOD, XK_x,     ACTION##stack, {.i = PREVSEL } }, \
   { MOD, XK_q,     ACTION##stack, {.i = 0 } }, \
   { MOD, XK_a,     ACTION##stack, {.i = 1 } }, \
@@ -95,13 +96,40 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static char dmenumon[2]            = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[]      = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static const char *termcmd[]       = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = {"S", "st", "-t", scratchpadname, "-g","100x40", NULL };
-static const char floattermname[] = "floatterm";
-static const char *floattermcmd[] = {"T", "st", "-t", floattermname, "-g","100x40", NULL };
+static const char floattermname[]  = "floatterm";
+static const char *floattermcmd[]  = {"T", "st", "-t", floattermname, "-g","100x40", NULL };
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "font",               STRING,  &font },
+		{ "font2",              STRING,  &font2 },
+		{ "dmenufont",          STRING,  &dmenufont },
+		{ "normbgcolor",        STRING,  &normbgcolor },
+		{ "normbordercolor",    STRING,  &normbordercolor },
+		{ "normfgcolor",        STRING,  &normfgcolor },
+		{ "selbgcolor",         STRING,  &selbgcolor },
+		{ "selbordercolor",     STRING,  &selbordercolor },
+		{ "selfgcolor",         STRING,  &selfgcolor },
+		{ "borderpx",          	INTEGER, &borderpx },
+		{ "gappx",          	  INTEGER, &gappx},
+		{ "urgborder",          INTEGER, &urgborder },
+		{ "snap",          		  INTEGER, &snap },
+		{ "topbar",          	  INTEGER, &topbar },
+		{ "nmaster",          	INTEGER, &nmaster },
+		{ "resizehints",       	INTEGER, &resizehints },
+    { "mfact",      	 	    FLOAT,   &mfact },
+    { "systraypinning",     INTEGER, &systraypinning },
+    { "systrayonleft",      INTEGER, &systrayonleft },
+    { "systrayspacing",     INTEGER, &systrayspacing },
+};
+
 static Key keys[] = {
   /* modifier                     key        function        argument */
   { MODKEY,                       XK_e,      spawn,          {.v = dmenucmd } },
@@ -138,12 +166,10 @@ static Key keys[] = {
   { MODKEY,                       XK_f,      togglefullscr,  {0} },
   { MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
   { MODKEY,                       XK_Tab,    view,           {0} },
-  { MODKEY,                       XK_bracketleft,      cycleattachdir,{.i = +1} },
-  { MODKEY,                       XK_bracketright,      cycleattachdir,{.i = -1 } },
+  { MODKEY,                       XK_bracketleft, cycleattachdir,{.i = +1 } },
+  { MODKEY,                       XK_bracketright,cycleattachdir,{.i = -1 } },
   { MODKEY|ShiftMask,             XK_w,      killclient,     {0} },
   { MODKEY,                       XK_r,      cyclelayout,    {.i = +1 } },
-  //{ MODKEY,                       XK_t,      cyclelayout,    {.i = -1 } },
-  //{ MODKEY,                       XK_space,  setlayout,      {0} },
   { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
   { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
   { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
@@ -162,7 +188,7 @@ static Key keys[] = {
   TAGKEYS(                        XK_7,                      6)
   TAGKEYS(                        XK_8,                      7)
   TAGKEYS(                        XK_9,                      8)
-  { MODKEY|ShiftMask,             XK_F4,      quit,           {0} },
+  { MODKEY|ShiftMask,             XK_F4,      quit,          {0} },
 };
 
 /* button definitions */
