506b1a6b9e104c5822dafae495e61cca9d9aa620
Daniel Soukhov <dsoukhov@gmail.com>
2021-08-29 21:54:29 -0400

    added dunst-cfg that respects xresources

--- autostart.sh
+++ autostart.sh
@@ -5,6 +5,7 @@ hsetroot -solid '#000000'
 #feh --bg-scale ~/Pictures/wallpaper/wp1.png &
 compton --vsync opengl-swc --backend glx -e 1.0 -i 1.0 -o 1.0 -b
 xsettingsd -c ~/.xsettingsd &
-dunst &
+pkill dunst
+dunst-cfg &
 udiskie &
 dwmblocks &
