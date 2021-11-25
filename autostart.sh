c458271be9a666ae2def203387c70f3be02c978a
Daniel Soukhov <dsoukhov@gmail.com>
2021-11-25 16:56:37 -0500

    don't save vol

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-pamixer --set-volume 40
+#pamixer --set-volume 40
 hsetroot -solid '#000000'
-#hsetroot -solid '#ffffff'
 #feh --bg-scale ~/Pictures/wallpaper/wp1.png &
 picom -CGb
 xsettingsd -c ~/.xsettingsd &
