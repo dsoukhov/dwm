d974954bac7520a1b08d0ecb0570c5b365b32a0e
Daniel Soukhov <dsoukhov@gmail.com>
2021-12-08 01:28:43 -0500

    updated keymaps + autostart

--- autostart.sh
+++ autostart.sh
@@ -3,11 +3,17 @@ pamixer --set-volume 40 && pkill -RTMIN+1 dwmblocks
 pamixer --source "alsa_input.pci-0000_00_1f.3.analog-stereo" --set-volume 20 && pkill -RTMIN+2 dwmblocks
 hsetroot -solid '#000000'
 #feh --bg-scale ~/Pictures/wallpaper/wp1.png &
+pkill clipmenud
 CM_DIR=~/.cache clipmenud &
+pkill picon
 picom -CGb
+pkill xsettingsd
 xsettingsd -c ~/.xsettingsd &
+pkill dunst
 dunst &
+pkill udiskie
 udiskie &
+pkill dwmblocks
 dwmblocks &
 pkill 'pinknoise'
 pinknoise &
