86f6c2a005c9f5d3db4949770679ea8b342bf91c
Daniel Soukhov <dsoukhov@gmail.com>
2021-12-14 15:29:42 -0500

    applied swallow patch

--- config.def.h
+++ config.def.h
@@ -43,12 +43,13 @@ static const Rule rules[] = {
    *	WM_CLASS(STRING) = instance, class
    *	WM_NAME(STRING) = title
    */
-  /* class      instance    title               tags-mask    isfloating     ispermanent  monitor  ignoreReqest scratch-key can-get-swallowed  grabfocus-on-urgent*/
-  {  "St",      NULL,       NULL,               0,           0,             0,           0,       0,            0,         1,                 0},
-  {  "st",      NULL,       NULL,               0,           0,             0,           0,       0,            0,         1,                 0},
-  {  NULL,      NULL,       "scratchpad",       0,           1,             1,           0,       0,           'S',        1,                 0},
-  {  NULL,      NULL,       "floatterm",        0,           1,             1,           0,       0,           'T',        1,                 0},
-  { "net-runelite-client-RuneLite", NULL, NULL, 0,           1,             0,           0,       1,            0,         0,                 1},
+  /* class      instance    title               tags-mask    isfloating     ispermanent  monitor  ignoreReqest scratch-key  grabfocus-on-urgent no-swallow*/
+  {  "St",      NULL,       NULL,               0,           0,             0,           0,       0,            0,          0,                  0},
+  {  "st",      NULL,       NULL,               0,           0,             0,           0,       0,            0,          0,                  0},
+  {  NULL,      NULL,       "scratchpad",       0,           1,             1,           0,       0,           'S',         0,                  0},
+  {  NULL,      NULL,       "floatterm",        0,           1,             1,           0,       0,           'T',         0,                  0},
+  { "net-runelite-client-RuneLite", NULL, NULL, 0,           1,             0,           0,       1,            0,          1,                  0},
+  {  NULL, NULL, "Event Tester",                0,           1,             0,           0,       1,            0,          1,                  1},
 };
 
 /* layout(s) */
