8185a2a6d28bc1d41b983c5b90a6f6d72351b3f9
Daniel Soukhov <dsoukhov@gmail.com>
2022-04-30 22:53:38 -0400

    scratchpads grab on urgent

--- config.def.h
+++ config.def.h
@@ -46,9 +46,9 @@ static const Rule rules[] = {
    */
   /* class      instance    title               tags-mask    isfloating     monitor  ignoreReqest grab-on-urgent scratch-key no-swallow   is-term*/
   {  "st",      NULL,       NULL,               0,           0,             0,       0,           1,             0,          0,           1},
-  {  NULL,      NULL,       "scratchpad",       0,           1,             0,       0,           0,            'S',         0,           1},
-  {  NULL,      NULL,       "floatterm",        0,           1,             0,       0,           0,            'T',         0,           1},
-  {  NULL,      NULL,       "st-vimmode",       0,           1,             0,       0,           1,             0,          0,           0},
+  {  NULL,      NULL,       "scratchpad",       0,           1,             0,       0,           1,            'S',         0,           1},
+  {  NULL,      NULL,       "floatterm",        0,           1,             0,       0,           1,            'T',         0,           1},
+  {  NULL,      NULL,       "st-vimmode",       0,           0,             0,       0,           1,             0,          0,           0},
   { "net-runelite-client-RuneLite", NULL, NULL, 0,           1,             0,       1,           1,             0,          0,           0},
   {  NULL, NULL, "Event Tester",                0,           0,             0,       0,           1,             0,          1,           0},
 };
