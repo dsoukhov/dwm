ddb67108b49c5df684a0c35fe059a2ae65fed6a3
Daniel Soukhov <dsoukhov@gmail.com>
2022-11-07 02:17:32 -0500

    autostart

--- autostart.sh
+++ autostart.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 pamixer --set-volume 40 && pkill -RTMIN+1 dwmblocks
-pamixer --source "alsa_input.pci-0000_00_1f.3.analog-stereo" --set-volume 20 && pkill -RTMIN+2 dwmblocks
-hsetroot -solid '#000000'
+pamixer --default-source --set-volume 20 && pkill -RTMIN+2 dwmblocks
+xsetroot -solid "#000000"
 #feh --bg-scale ~/Pictures/wallpaper/wp1.png &
 pkill clipmenud
 CM_DIR=~/.cache clipmenud &
-pkill picom
-#picom -CGb
-picom -b
+# pkill picom
+# picom -b
 pkill xsettingsd
 xsettingsd -c ~/.xsettingsd &
 pkill dunst
@@ -16,7 +15,7 @@ pkill dwmblocks
 dwmblocks &
 pkill 'pinknoise'
 pinknoise &
-pkill autorandr-launcher
+# pkill autorandr-launcher
 # pkill autorandr
-autorandr-launcher -d
+# autorandr-launcher -d
 # autorandr -c
