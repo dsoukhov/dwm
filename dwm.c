c6cd43c9bb79d672b244cd6251cc59eb7c5762b5
Daniel Soukhov <dsoukhov@gmail.com>
2022-12-08 16:50:14 -0500

    grabfocus w/ deck works correctly

--- dwm.c
+++ dwm.c
@@ -2987,6 +2987,11 @@ grabfocus(Client *c)
     Client *fs = selmon->pertag->fullscreens[selmon->pertag->curtag];
     if (fs && fs != c)
       setfullscreen(fs, 0, 0);
+    if (selmon->lt[selmon->sellt]->arrange == deck) {
+      detachstack(c);
+      attachstack(c);
+      restack(selmon);
+    }
     focus(c);
   }
 }
